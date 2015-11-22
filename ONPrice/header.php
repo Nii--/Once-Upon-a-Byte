f8fcb4c53c66cd2b66418390f7074d7ac7b50ab3
Nii-- <retxin02@gmail.com>
2015-11-22 08:28:04 +0800

    initial commit

---
+++ ONPrice/header.php
@@ -0,0 +1,5 @@
+<div>
+    <div id="head">
+        <img id="logo" src="./graphics/logo.png">
+    </div>
+</div>
---
+++ ONPrice/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html>
+    <head>
+        <title>ONPrice - Price in a bit</title>
+        <link rel="stylesheet" href="./style/default.css">
+    </head>
+    <body>
+        <!--div class="block">
+            <img class="iblock" src="./graphics/food.png" alt="Food"/>
+        </div>
+        <div class="blocka">
+            <img class="iblock" src="./graphics/gadget.png" alt="Gadget"/>
+        </div>
+        <div class="block">
+            <img class="iblock" src="./graphics/outfit.png" alt="Outfits"/>
+        </div>
+        <div class="blocka">
+            <img class="iblock" src="./graphics/appliances.png" alt="Appliances"/>
+        </div>
+        <div class="block">
+            <img class="iblock" src="./graphics/furniture.png" alt="Furniture"/>
+        </div>
+        <div class="blocka">
+            <img class="iblock" src="./graphics/supplies.png" alt="Supplies"/>
+        </div>
+        <div class="block">
+            <img class="iblock" src="./graphics/toys.png" alt="Toys and Hobby"/>
+        </div>
+        <div class="block">
+            <img class="iblock" src="./graphics/etc.png" alt="etc"/>
+        </div-->
+        
+        <?php
+            require './header.php';
+        ?>
+        
+        <table class="centered">
+            <tr>
+                <td class="block">
+                    <img class="iblock" src="./graphics/food.png" alt="Food"/>
+                </td>
+                <td class="blocka">
+                    <img class="iblocka" src="./graphics/gadget.png" alt="Gadget"/>
+                </td>
+                <td class="block">
+                    <img class="iblock" src="./graphics/outfit.png" alt="Outfits"/>
+                </td>
+                <td class="blocka">
+                    <img class="iblocka" src="./graphics/appliances.png" alt="Appliances"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="block">
+                    <img class="iblocka" src="./graphics/furniture.png" alt="Furniture"/>
+                </td>
+                <td class="blocka">
+                    <img class="iblock" src="./graphics/supplies.png" alt="Supplies"/>
+                </td>
+                <td class="block">
+                    <img class="iblocka" src="./graphics/toys.png" alt="Toys and Hobby"/>
+                </td>
+                <td class="blocka">
+                    <img class="iblock" src="./graphics/etc.png" alt="etc"/>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
